6b7ccedb21179664709cabddf779abb41994594c
Justin <jwhittington92@yahoo.com>
2018-05-30 20:48:55 -0400

    fix?

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
 
 
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Ezra0827",
   database: "burgers_db"
+
 });
+};
 
 
 connection.connect(function (err) {
